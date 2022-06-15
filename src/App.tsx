8bb53f23011d8774d24d2d5ff095b0542c833a65
guilhermeforprojeto <guylhermesantos@hotmail.com>
2022-06-15 02:14:36 -0300

    Incrementar e deccrementar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
-  return <div className="App">BORA CODA! LEIA O README.MD</div>;
+  const [count, setCount] = useState<number | undefined>(undefined);
+  const [name, setName] = useState<string | undefined>(undefined);
+  return <div className="App"><h2>BORA CODA! LEIA O README.MD</h2>
+  <p>{name || 'Sem nome defido'}</p>
+  <input onChange={(event) => setName(event.target.value)}></input>
+  <h1>{count}</h1>
+  <button onClick={() => setCount((count || 0) + 1)} >Incrementar</button>
+  <button onClick={() => setCount((count || 0)- 1)} >Decrementar</button>
+  </div>;
 }
 
 export default App;
